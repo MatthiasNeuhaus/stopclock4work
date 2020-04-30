6caf6420b00d5a3f6e3adf409f92b626b509b8d3
Neuhaus Matthias (uidv1094) <Matthias.Neuhaus@continental-corporation.com>
2020-04-30 08:13:08 +0200

    add actions for every button

--- stopclock4work.ps1
+++ stopclock4work.ps1
@@ -55,6 +55,9 @@ $CountupPause.ForeColor = "red"
 $CountupPause.TextAlign = "MiddleCenter"
 $MainWindow.Controls.Add($CountupPause)
 
+function WriteToCsv () {
+    #$StartTime.Date+$StartTime.Hour | Export-Csv -Path .\timesheet.csv -Delimiter ';'
+}
 
 function GetLogonStatus () {
     try {
@@ -79,17 +82,24 @@ $stopclock = {
         $Countup.Text = $script:NormalTime.ToString("hh\:mm\:ss");
     }
     else {
-        $Time = Get-Date;
-        $script:PauseTime = New-TimeSpan –Start $script:PauseStartTime –End $Time;
-        $script:PauseTime = $script:PauseTime + $script:TotalPauseTime;
-        $CountupPause.Text = $script:PauseTime.ToString("hh\:mm\:ss");
-        
         if ($script:Pause -eq 2) {
             $retval = GetLogonStatus
             if ( $retval -eq 0) {
                 $script:Pause = 0
+                
+                $Time = Get-Date;
+                $script:PauseTime = New-TimeSpan –Start $script:PauseStartTime –End $Time;
+                $script:PauseTime = $script:PauseTime + $script:TotalPauseTime;
+                $CountupPause.Text = $script:PauseTime.ToString("hh\:mm\:ss");
+
                 $script:TotalPauseTime = $script:PauseTime
             }
+        }
+        else {
+            $Time = Get-Date;
+            $script:PauseTime = New-TimeSpan –Start $script:PauseStartTime –End $Time;
+            $script:PauseTime = $script:PauseTime + $script:TotalPauseTime;
+            $CountupPause.Text = $script:PauseTime.ToString("hh\:mm\:ss");
         }
     }
 }
@@ -108,7 +118,11 @@ $StopClose.Text = "Stop and Close"
 $StopClose.Width = $Widht
 $StopClose.Height = $Height
 $StopClose.Location = New-Object System.Drawing.Size(0, $Height)
-$StopClose.Add_Click( { $timer.Enabled = $False })
+$StopClose.Add_Click( { 
+        $timer.Enabled = $False 
+        WriteToCsv
+        [void] $MainWindow.Close()
+    })
 $MainWindow.Controls.Add($StopClose)
 
 $StopHybernate = New-Object System.Windows.Forms.Button
@@ -116,7 +130,11 @@ $StopHybernate.Text = "Stop and Hybernate"
 $StopHybernate.Width = $Widht
 $StopHybernate.Height = $Height
 $StopHybernate.Location = New-Object System.Drawing.Size(0, (2 * $Height))
-$StopHybernate.Add_Click( { $script:Pause = 2 })
+$StopHybernate.Add_Click( { 
+        $timer.Enabled = $False
+        WriteToCsv
+        rundll32.exe powrprof.dll, SetSuspendState 0, 1, 0
+    })
 $MainWindow.Controls.Add($StopHybernate)
 
 $StopShutdown = New-Object System.Windows.Forms.Button
@@ -124,7 +142,12 @@ $StopShutdown.Text = "Stop and Shutdown"
 $StopShutdown.Width = $Widht
 $StopShutdown.Height = $Height
 $StopShutdown.Location = New-Object System.Drawing.Size(0, (3 * $Height))
-$StopShutdown.Add_Click( { [void] $NewWindow.ShowDialog() })
+$StopShutdown.Add_Click( { 
+        $timer.Enabled = $False
+        WriteToCsv
+        shutdown /s
+        [void] $MainWindow.Close()
+    })
 $MainWindow.Controls.Add($StopShutdown)
 
 $PauseButton = New-Object System.Windows.Forms.Button
@@ -134,7 +157,8 @@ $PauseButton.Height = $Height
 $PauseButton.Location = New-Object System.Drawing.Size($Widht, $Height)
 $PauseButton.Add_Click( { 
         $script:Pause = 1
-        $script:PauseStartTime = Get-Date })
+        $script:PauseStartTime = Get-Date 
+    })
 $MainWindow.Controls.Add($PauseButton)
 
 $Resume = New-Object System.Windows.Forms.Button
@@ -144,7 +168,8 @@ $Resume.Height = $Height
 $Resume.Location = New-Object System.Drawing.Size($Widht, (2 * $Height))
 $Resume.Add_Click( { 
         $script:Pause = 0
-        $script:TotalPauseTime = $script:PauseTime })
+        $script:TotalPauseTime = $script:PauseTime 
+    })
 $MainWindow.Controls.Add($Resume)
 
 $PauseLock = New-Object System.Windows.Forms.Button
@@ -153,12 +178,11 @@ $PauseLock.Width = $Widht
 $PauseLock.Height = $Height
 $PauseLock.Location = New-Object System.Drawing.Size($Widht, (3 * $Height))
 $PauseLock.Add_Click( { 
-        
         $script:PauseStartTime = Get-Date
         rundll32.exe user32.dll, LockWorkStation
-        Start-Sleep -Seconds 10;
+        Start-Sleep -Seconds 5; # wait till user is really loged of
         $script:Pause = 2
-     })
+    })
 $MainWindow.Controls.Add($PauseLock)
 
 
