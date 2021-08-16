d62dad261a260411a9db9524cee1626d6b50fb86
Neuhaus Matthias (uidv1094) <Matthias.Neuhaus@continental-corporation.com>
2021-08-16 07:18:39 +0200

    sadly default has to be changed to not automatically detect a locked workstation

--- DataSource/OptionsV0.ps1
+++ DataSource/OptionsV0.ps1
@@ -1,5 +1,5 @@
-# By default, StopClock4Work starts a break when workstation is locked
+# By default, StopClock4Work starts a break when workstation is locked (currently default is changed to False - restart is otherwise counted as break)
 # With this option you can disable this
 # Valid parameters are $True / $False
 
-$AutoStopByLock = $True
+$AutoStopByLock = $False
